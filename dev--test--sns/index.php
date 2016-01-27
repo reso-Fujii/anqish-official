310ae5be4ed5c4084d251b20da52f95e97d7fc92
[reso-fujii] <[fujii@re-solution.co.jp]>
2016-01-27 11:39:46 +0900

    Twitter動作確認。 fb要調整。
    とりあえず、log保存用コミット。

---
+++ dev--test--sns/index.php
@@ -0,0 +1,221 @@
+<?php
+?>
+<!DOCTYPE html>
+<!--[if IE 6]> <html class="no-js lt-ie9 lt-ie8 lt-ie7 eq-ie6" lang="ja"> <![endif]-->
+<!--[if IE 7]> <html class="no-js lt-ie9 lt-ie8 eq-ie7" lang="ja"> <![endif]-->
+<!--[if IE 8]> <html class="no-js lt-ie9 eq-ie8" lang="ja"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="ja"> <!--<![endif]-->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="Content-Language" content="ja" />
+<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1" />
+<meta http-equiv="Content-Script-Type" content="text/javascript">
+<meta http-equiv="Content-Style-Type" content="text/css">
+<meta http-equiv="imagetoolbar" content="no" />
+<meta name="Keywords" content="ANQish,アンキッシュ,webデザイナー,ホームページ制作">
+<meta name="description" content="都内在住のWebサイト制作者の個人サイト ANQish「アンキッシュ」">
+<meta name="viewport" content="user-scalable=0,width=device-width,initial-scale=1,maximum-scale=1">
+<title>ANQish.com| Web Desighn</title>
+   
+    <!--  OGP fb -->     
+    <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#">
+    <meta content="702329666575028" property="fb:app_id">
+
+        <!--  OGP Base -->
+        <meta content="OGP設定テスト中！" property="og:title">
+        <meta content="article" property="og:type">
+        <meta content="OGPを設定してしっかりSNSでたくさんの情報を発信できるようにしよう。多くの情報を発信することにより、拡散された情報がより魅力的なものになるぞっ。" property="og:description">
+        <meta content="http://anqish.com/" property="og:url">
+        <meta content="http://anqish.com/images/thanks.jpg" property="og:image">
+        <meta content="ANQish [アンキッシュ]" property="og:site_name">
+        <meta content="ja_JP" property="og:locale">
+
+        <!--  OGP twitter -->
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:site" content="@Fesp_com">
+        <meta name="twitter:creator" content="@Fesp_com">
+            <meta name="twitter:domain" content="anqish.com">
+        <meta name="twitter:title" content="OGP tests. Trial and Error">
+        <meta name="twitter:OGP tests. Trial and Error 発信できるように設定テスト中です。テスト発信を行います。">
+        <meta name="twitter:image:src" content="http://anqish.com/images/thanks.jpg">
+
+
+        
+
+
+<link rel="stylesheet" href="../css/import.css" type="text/css" media="all">
+<link rel="shortcut icon" href="./favicon.ico">
+
+    <!-- Google+ --  head 内か、body 終了タグの直前に次のタグを貼り付けてください。 -->
+    <script src="https://apis.google.com/js/platform.js" async defer>
+        {lang: 'ja'}
+    </script>
+
+    <!-- 共有ボタン を表示したい位置に次のタグを貼り付けてください。 -->
+    <div class="g-plus" data-action="share" data-annotation="vertical-bubble" data-height="60"></div>
+
+</head>
+<body>
+    <div id="fb-root"></div>
+    <script>
+        window.fbAsyncInit = function() {
+            FB.init({
+                appId      : '702329666575028',
+                xfbml      : true,
+                version    : 'v2.5'
+            });
+        };
+
+        (function(d, s, id){
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {return;}
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    </script>
+
+<div id="wrapper">
+
+  <div id="mask">
+
+    <div class="keyboardable" title="キーボードでページ移動が可能です。">Keyboard Move</div>
+    
+             <nav class="navPane">
+                 <div id="LogoArea"><a href="/#Home"><img id="Logo"  alt="Logo" src="../images/spacer.gif"><span>ANQish</span></a></div>
+                    <ul class="links-to-floor">
+                        <li><a href="http://anqish.com/#Home">Home<span>Wellcome</span></a></li>
+                        <li><a href="http://anqish.com/#About">About<span>What site</span></a></li>
+                        <li><a href="http://anqish.com/#Works">Works<span>Design</span></a></li>
+                        <li><a href="http://anqish.com/#Blog">Blog<span>Blog</span></a></li>
+                        <li><a href="http://anqish.com/#Access">Access<span>Access</span></a></li>
+                        <li><a href="http://anqish.com/#Contact">Contact<span>Contact</span></a></li>
+                    </ul>
+                </nav>
+                        <div class="navScrollMask" style="right: 15px;"></div>
+      
+
+<div id="keymove">
+
+  <!-- Thanks -->
+
+    <section id="Thanks" class="content">
+    <article class="container">
+     <div id="welcome">
+         <div class="sns__share">
+
+             <!-- シェアボタン [ここからコピー] -->
+             <div class="social-area-syncer">
+                 <ul class="social-button-syncer">
+                     <!-- Twitter ([Tweet]の部分を[ツイート]にすると日本語にできます) -->
+                     <li class="sc-tw"><a data-url="http://anqish.com/" href="https://twitter.com/share" class="twitter-share-button" data-lang="ja" data-count="vertical" data-dnt="true" target="_blank"><svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414"><path d="M16 3.038c-.59.26-1.22.437-1.885.517.677-.407 1.198-1.05 1.443-1.816-.634.375-1.337.648-2.085.795-.598-.638-1.45-1.036-2.396-1.036-1.812 0-3.282 1.468-3.282 3.28 0 .258.03.51.085.75C5.152 5.39 2.733 4.084 1.114 2.1.83 2.583.67 3.147.67 3.75c0 1.14.58 2.143 1.46 2.732-.538-.017-1.045-.165-1.487-.41v.04c0 1.59 1.13 2.918 2.633 3.22-.276.074-.566.114-.865.114-.21 0-.416-.02-.617-.058.418 1.304 1.63 2.253 3.067 2.28-1.124.88-2.54 1.404-4.077 1.404-.265 0-.526-.015-.783-.045 1.453.93 3.178 1.474 5.032 1.474 6.038 0 9.34-5 9.34-9.338 0-.143-.004-.284-.01-.425.64-.463 1.198-1.04 1.638-1.7z" fill="#fff" fill-rule="nonzero"/></svg><span>ツイート</span></a></li>
+
+                     <!-- Facebook シェア -->
+                     <li class="sc-fb"><a class="facebook-btn" href="//www.facebook.com/share.php?app_id=702329666575028&amp;ref=plugin&amp;u=http://anqish.com/dev--test--sns/index.php" onclick="window.open(this.href, &#39;FBwindow&#39;, &#39;width=650, height=450, menubar=no, toolbar=no, scrollbars=yes&#39;); return false;"><i class="fa fa-facebook-square"></i><span>シェア</span></a></li>
+                    
+                     <!-- Facebook いいね -->
+                     <li class="sc-fb"><div class="fb-like" data-href="http://anqish.com/" data-layout="box_count" data-action="like" data-show-faces="true" data-share="false"></div></li>
+                                         
+                     <!-- Google+ -->
+                     <li><div data-href="http://anqish.com/" class="g-plusone" data-size="tall"></div></li>
+
+
+                     <!-- LINE  -->
+                     <li class="sc-li"><a href="http://line.me/R/msg/text/?http://anqish.com/"><img src="../images/linebutton_36x60.png" width="36" height="60" alt="LINEに送る" class="sc-li-img"></a></li>
+                 </ul>
+
+                 <!-- Facebook用 -->
+                 <div id="fb-root"></div>
+
+             </div>
+             <!-- シェアボタン [ここまでコピー] -->
+             
+             
+             
+             <!-- シェアボタン [ここからコピー] -->
+
+             <?php
+             $share_url_syncer = "http://anqish.com/";		//シェア対象のURLアドレスを指定する (HTML部分は変更不要)
+             ?>
+
+             <div class="social-area-syncer">
+                 <ul class="social-button-syncer">
+                     <!-- Twitter [Tweet]の部分を[ツイート]に変更することで日本語仕様になります。 -->
+                     <li class="sc-tw"><a data-url="<?php echo $share_url_syncer ; ?>" href="https://twitter.com/share" class="twitter-share-button" data-lang="ja" data-count="vertical" data-dnt="true" target="_blank"><svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414"><path d="M16 3.038c-.59.26-1.22.437-1.885.517.677-.407 1.198-1.05 1.443-1.816-.634.375-1.337.648-2.085.795-.598-.638-1.45-1.036-2.396-1.036-1.812 0-3.282 1.468-3.282 3.28 0 .258.03.51.085.75C5.152 5.39 2.733 4.084 1.114 2.1.83 2.583.67 3.147.67 3.75c0 1.14.58 2.143 1.46 2.732-.538-.017-1.045-.165-1.487-.41v.04c0 1.59 1.13 2.918 2.633 3.22-.276.074-.566.114-.865.114-.21 0-.416-.02-.617-.058.418 1.304 1.63 2.253 3.067 2.28-1.124.88-2.54 1.404-4.077 1.404-.265 0-.526-.015-.783-.045 1.453.93 3.178 1.474 5.032 1.474 6.038 0 9.34-5 9.34-9.338 0-.143-.004-.284-.01-.425.64-.463 1.198-1.04 1.638-1.7z" fill="#fff" fill-rule="nonzero"/></svg><span>ツイート</span></a></li>
+
+                     <!-- Facebook シェア -->
+                     <li class="sc-fb"><a class="facebook-btn" href="//www.facebook.com/share.php?app_id=702329666575028&amp;ref=plugin&amp;u=http://anqish.com/dev--test--sns/index.php" onclick="window.open(this.href, &#39;FBwindow&#39;, &#39;width=650, height=450, menubar=no, toolbar=no, scrollbars=yes&#39;); return false;"><i class="fa fa-facebook-square"></i><span>シェア</span></a></li>
+                                         
+                     <!-- Facebook いいね -->
+                     <li class="sc-fb"><div class="fb-like" data-href="<?php echo $share_url_syncer ; ?>" data-layout="box_count" data-action="like" data-show-faces="true" data-share="false"></div></li>
+
+                     <!-- Google+ -->
+                     <li><div data-href="<?php echo $share_url_syncer ; ?>" class="g-plusone" data-size="tall"></div></li>
+
+
+                     <!-- LINE  -->
+                     <li class="sc-li"><a href="http://line.me/R/msg/text/?<?php echo rawurlencode($share_url_syncer); ?>"><img src="../images/linebutton_36x60.png" width="36" height="60" alt="LINEに送る" class="sc-li-img"></a></li>
+                 </ul>
+
+                 <!-- Facebook用 -->
+                 <div id="fb-root"></div>
+
+             </div>
+             <!-- シェアボタン [ここまでコピー] -->
+         </div>
+
+    </div>
+        
+        
+     <div class="footer">
+         <ul class="links-to-floor">
+                        <li><a href="http://anqish.com/#Home">Home<span>Wellcome</span></a></li>
+                        <li><a href="http://anqish.com/#About">About<span>What site</span></a></li>
+                        <li><a href="http://anqish.com/#Works">Works<span>Design</span></a></li>
+                        <li><a href="http://anqish.com/#Blog">Blog<span>Blog</span></a></li>
+                        <li><a href="http://anqish.com/#Access">Access<span>Access</span></a></li>
+                        <li><a href="http://anqish.com/#Contact">Contact<span>Contact</span></a></li>
+         </ul>
+
+         <div class="copyright">© 2015 ANQish.com</div>
+     </div>
+  </article>
+  </section>   
+  <!-- //Thanks -->
+
+
+    
+    
+</div>  <!-- //keymove -->      
+      
+      
+</div>  <!-- //mask -->
+</div>  <!-- //wapper -->
+
+
+<!--  (^• ◡•^)  (＠ᴥ＠ʋ) -->
+
+
+<script src="http://code.jquery.com/jquery-1.9.0.js"></script>
+<script src="js/plugin.min.js"></script>
+<script src="js/main.min.js"></script>
+<!--[if lt IE 9]>
+<script src="js/html5shiv.js"></script>
+<![endif]-->
+    
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-35379509-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+  </body>
+</html>
